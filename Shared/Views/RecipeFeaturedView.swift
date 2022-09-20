cfe84fdd9d476c63f312614115c7d27f10b5ec47
Monika Herain <monika.herain@gmail.com>
2022-09-20 19:47:15 +0000

    change all the fonts

--- Shared/Views/RecipeFeaturedView.swift
+++ Shared/Views/RecipeFeaturedView.swift
@@ -21,7 +21,7 @@ struct RecipeFeaturedView: View {
                 .bold()
                 .padding(.leading)
                 .padding(.top, 40)
-                .font(.largeTitle)
+                .font(Font.custom("Avenir Heavy", size: 24))
             
             GeometryReader { geo in
                 
@@ -50,6 +50,7 @@ struct RecipeFeaturedView: View {
                                             .clipped()
                                         Text(model.recipes[index].name)
                                             .padding(5)
+                                            .font(Font.custom("Avenir", size: 15))
                                     }
                                 }
                             })
@@ -71,10 +72,11 @@ struct RecipeFeaturedView: View {
             
             VStack (alignment: .leading, spacing: 3) {
                 Text("Preparation time:")
-                    .font(.headline)
+                    .font(Font.custom("Avenir Heavy", size: 16))
                 Text(model.recipes[tabSelectionIndex].prepTime)
+                    .font(Font.custom("Avenir", size: 15))
                 Text("Highlights")
-                    .font(.headline)
+                    .font(Font.custom("Avenir Heavy", size: 16))
                 RecipeHighlights(highlights: model.recipes[tabSelectionIndex].highlights)
             }
             .padding([.leading, .bottom])
--- Shared/Views/RecipeListView.swift
+++ Shared/Views/RecipeListView.swift
@@ -22,7 +22,7 @@ struct RecipeListView: View {
                 Text("All Recipes")
                     .bold()
                     .padding(.top, 40)
-                    .font(.largeTitle)
+                    .font(Font.custom("Avenir Heavy", size: 24))
                 
                 ScrollView {
                     LazyVStack(alignment: .leading) {
@@ -42,7 +42,7 @@ struct RecipeListView: View {
                                         VStack (alignment: .leading) {
                                             Text(r.name)
                                                 .foregroundColor(.black)
-                                                .bold()
+                                                .font(Font.custom("Avenir Heavy", size: 16))
                                             RecipeHighlights(highlights: r.highlights)
                                                 .foregroundColor(.black)
                                         }
